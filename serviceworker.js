8e75bac7eeb38a1276934eaacd14614c33203a57
Yoondoyoung <72640656+Yoondoyoung@users.noreply.github.com>
2024-04-07 04:31:38 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ serviceworker.js
@@ -0,0 +1,36 @@
+const rankFileMedia = "rank-file-media-v1";
+const assets = [
+  "/",
+  "/main.html",
+  "/search.html",
+  "/story.html",
+  "/js/app.js",
+  "/poll/pollpage.html",
+  "/profile/profilepage.html",
+  "/login/loginPopup.html",
+  "/login/signIn.html",
+  "/login/signUp.html",
+  "/login/signInReporterContract.html",
+  "/login/thankyou.html",
+  "/login/js/loginPopup.js",
+  "/login/js/signIn.js",
+  "/cards/caleb.html",
+  "/cards/contessa.html",
+
+];
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+      caches.open(rankFileMedia).then(cache => {
+        cache.addAll(assets);
+      })
+    );
+  });
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
